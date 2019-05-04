e77cee63430f91cf91ddc057ac1059c317e5c6b6
Condzi <Condzi@users.noreply.github.com>
2019-05-04 18:38:35 +0200

    init

---
+++ source/pch.cpp
@@ -0,0 +1,6 @@
+/*
+	Conrad 'Condzi' Kubacki 2019
+	https://github.com/condzi
+*/
+
+#include "pch.hpp"
---
+++ source/pch.hpp
@@ -0,0 +1,11 @@
+/*
+	Conrad 'Condzi' Kubacki 2019
+	https://github.com/condzi
+*/
+
+#pragma once
+
+int main()
+{
+	return 0;
+}
