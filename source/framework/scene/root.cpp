29d9df217f3ce68caa9546d9d9d2d13f38c358be
Condzi <Condzi@users.noreply.github.com>
2019-05-07 21:54:21 +0200

    add Root::change_scene

--- source/framework/scene/root.cpp
+++ source/framework/scene/root.cpp
@@ -5,6 +5,7 @@
 
 #include "pch.hpp"
 
+#include "framework/common/assertions.hpp"
 #include "root.hpp"
 #include "application.hpp"
 
@@ -18,6 +19,10 @@ void Root::handle_input( sf::Event const& event )
 
 void Root::update( r32 delta )
 {
+	if ( active_scene is nullptr and pending_scene is_not nullptr ) 
+		active_scene = attach( change_owner( pending_scene ) );
+
+
 	time_since_update += delta;
 
 	if ( time_since_update >= fps_update_interval ) {
@@ -25,5 +30,28 @@ void Root::update( r32 delta )
 
 		engine_log_info( "+++++ FPS: {0:.1f} +++++", 1 / delta );
 	}
+}
+
+void Root::change_scene( Node_Ptr&& scene_to_switch )
+{
+	report_error_if( scene_to_switch is nullptr )
+	{
+		return;
+	}
+
+	report_warning_if( pending_scene is_not nullptr )
+	{
+		engine_log_warning( "There was one scene that was going to change to ({}). Changing to: {}.", pending_scene->name, scene_to_switch->name );
+	}
+
+	if ( active_scene is nullptr ) {
+		active_scene = attach( change_owner( scene_to_switch ) );
+		return;
+	}
+
+	engine_log_info( "Scene transition: \"{}\" => \"{}\".", active_scene->name, scene_to_switch->name );
+	pending_scene = change_owner( scene_to_switch );
+	active_scene->queue_for_delete();
+	active_scene = nullptr;
 }
 }
--- source/framework/scene/root.hpp
+++ source/framework/scene/root.hpp
@@ -22,9 +22,14 @@ public:
 	void handle_input( sf::Event const& event ) override;
 	void update( r32 delta ) override;
 
+	void change_scene( Node_Ptr&& scene );
+
 private:
+	Node* active_scene{ nullptr };
+	Node_Ptr pending_scene{ nullptr };
+
 	r32 time_since_update{ 0 };
 	// in seconds
-	static constexpr r32 fps_update_interval{ 1 };
+	static constexpr r32 fps_update_interval{ 3 };
 };
 }
