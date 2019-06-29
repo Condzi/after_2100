0380286e390dcd8875381a448c6d1165516e9b3a
condzi <Condzi@users.noreply.github.com>
2019-06-29 18:21:03 +0200

    add area for detecting intersection of two shapes

---
+++ source/framework/scene/area.cpp
@@ -0,0 +1,69 @@
+/*
+	Conrad 'Condzi' Kubacki 2019
+	https://github.com/condzi
+*/
+
+#include "pch.hpp"
+
+#include "area.hpp"
+
+namespace con
+{
+void Area::set_circle_shape( Circle_Shape const& circle )
+{
+	shape_set = true;
+	shape = circle;
+}
+
+void Area::set_rectangle_shape( Rectangle_Shape const& rectangle )
+{
+	shape_set = true;
+	shape = rectangle;
+}
+
+auto Area::is_shape_set() const -> bool
+{
+	return shape_set;
+}
+
+auto Area::get_shape_variant() const -> Shape_Variant const&
+{
+	return shape;
+}
+
+void Area::draw( Drawing_Set& drawing_set )
+{
+	if ( shape_set is false or draw_shape is false )
+		return;
+
+	std::visit( visitor_overload{
+				[&]( Rectangle_Shape const& rectangle ) {
+					visual_representation = sf::RectangleShape{};
+					auto& rect_to_draw = std::get<sf::RectangleShape>( visual_representation );
+
+					rect_to_draw.setPosition( rectangle.position );
+					rect_to_draw.setSize( rectangle.size );
+					rect_to_draw.setFillColor( sf::Color::Transparent );
+					rect_to_draw.setOutlineThickness( 1.0px );
+					rect_to_draw.setOutlineColor( shape_color );
+				},
+				[&]( Circle_Shape const& circle ) {
+					visual_representation = sf::CircleShape{};
+					auto& circle_to_draw = std::get<sf::CircleShape>( visual_representation );
+
+					// Center the circle just for comfort.
+					circle_to_draw.setOrigin( circle.radious / 2, circle.radious / 2 );
+					circle_to_draw.setPosition( circle.center );
+					circle_to_draw.setRadius( circle.radious );
+					circle_to_draw.setFillColor( sf::Color::Transparent );
+					circle_to_draw.setOutlineThickness( 1.0px );
+					circle_to_draw.setOutlineColor( shape_color );
+				}
+				}, shape );
+
+	std::visit(
+		[&]( auto&& drawable ) {
+			drawing_set.add_drawable( drawable, layer );
+		}, visual_representation );
+}
+}
---
+++ source/framework/scene/area.hpp
@@ -0,0 +1,45 @@
+/*
+	Conrad 'Condzi' Kubacki 2019
+	https://github.com/condzi
+*/
+#pragma once
+
+#include <SFML/Graphics/CircleShape.hpp>
+#include <SFML/Graphics/RectangleShape.hpp>
+
+#include "framework/common/shapes.hpp"
+#include "framework/common/signal.hpp"
+#include "node.hpp"
+
+namespace con
+{
+class Area :
+	public Node
+{
+	CLASS_DEF( Area );
+
+public:
+	// when other area enters
+	Signal<Area&> s_area_entered;
+
+	bool      draw_shape{ false };
+	sf::Color shape_color{ sf::Color::Red };
+	s32       layer{ 0 };
+
+	s32       collision_layer{ 0 };
+
+	void set_circle_shape( Circle_Shape const& circle );
+	void set_rectangle_shape( Rectangle_Shape const& rectangle );
+
+	[[nodiscard]] auto is_shape_set()	   const -> bool;
+	[[nodiscard]] auto get_shape_variant() const -> Shape_Variant const&;
+
+	void draw( Drawing_Set& drawing_set ) override;
+
+private:
+	std::variant<sf::RectangleShape, sf::CircleShape> visual_representation;
+
+	bool          shape_set{ false };
+	Shape_Variant shape;
+};
+}
