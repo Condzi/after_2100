d444c49e185fa1baa63b2562718313df3fd442a2
condzi <Condzi@users.noreply.github.com>
2019-07-31 13:44:47 +0200

    move definitions from pch to typedefs.hpp, add compile_constant

--- source/framework/common/typedefs.hpp
+++ source/framework/common/typedefs.hpp
@@ -21,3 +21,61 @@ using u64 = uint64_t;
 using r32 = float;
 using r64 = double;
 }
+
+#define CLASS_DEF( class_ )                                        \
+public:                                                            \
+                                                                   \
+compile_constant get_class_name_static()                           \
+{                                                                  \
+	return #class_;                                                \
+}                                                                  \
+                                                                   \
+virtual std::string get_class_name() const                         \
+{                                                                  \
+	return get_class_name_static();                                \
+}                                                                  \
+                                                                   \
+inline static auto instantiate()                                   \
+{                                                                  \
+	return std::make_unique<class_>();                             \
+}                                                                  \
+private:
+
+
+inline constexpr con::r32 operator"" px( long double val )
+{
+	return static_cast<con::r32>( val );
+}
+
+inline constexpr con::r32 operator"" sec( long double val )
+{
+	return static_cast<con::r32>( val );
+}
+// Automaticly sets correct degrees value (400 -> 400 - 360 = 40)
+inline constexpr con::r32 operator"" deg( long double val )
+{
+	if ( val > 360 ) {
+		auto const as_integer{ static_cast<con::s32>( val ) };
+		return static_cast<con::r32>( val ) - 360 * ( as_integer % 360 );
+	}
+
+	// add support for it if needed
+	if( val < 0 ) assert( "Negative degrees are not supported." );
+
+	return static_cast<con::r32>( val );
+}
+
+#define is ==
+#define is_not !=
+#define returned is
+#define constant auto const
+#define compile_constant static constexpr auto
+#define change_owner( uptr ) std::move( uptr )
+#define unused( x ) ( ( void )( x ) )
+
+namespace con
+{
+// Helps with std::variant visiting. provides possibility to pass multiple lambdas. see: https://en.cppreference.com/w/cpp/utility/variant/visit
+template<class... Ts> struct visitor_overload : Ts... { using Ts::operator()...; };
+template<class... Ts> visitor_overload( Ts... ) -> visitor_overload<Ts...>;
+}
