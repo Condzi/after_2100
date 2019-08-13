4e4d73350a0de5944502301cd120e6909ef1e116
condzi <Condzi@users.noreply.github.com>
2019-08-13 07:40:26 +0200

    optimize report_and_break

--- source/framework/common/assertions.cpp
+++ source/framework/common/assertions.cpp
@@ -20,25 +20,28 @@ bool force_break( bool condition )
 	return condition;
 }
 
-bool report_and_break( spdlog::level::level_enum log_level, bool should_break, bool condition, const char * condition_string, const char * file, s64 line )
+bool report_and_break( spdlog::level::level_enum log_level, bool should_break, bool condition, const char* condition_string, const char* file, s64 line )
 {
-	// We don't want C:/the/whole/path/, just the source/path/
-	std::string local_path{ file };
-	local_path = local_path.substr( local_path.find( "source" ) );
+	if ( condition is false )
+		return false;
+
+	static constant local_path_begin = [&]() {
+		return std::string_view{ file }.find( "source" );
+	}( );
+
+	constant file_path_length = strlen( file );
+	std::string_view const local_path{ file+local_path_begin, file_path_length - local_path_begin };
 
 	auto& logger = Debug_Log::get_instance().engine_logger;
 
-	if ( condition ) {
+	logger->log( log_level, "==============================" );
+	logger->log( log_level, "Expression: {}", condition_string );
+	logger->log( log_level, "File: {}", local_path );
+	logger->log( log_level, "Line: {}", line );
+	logger->log( log_level, "==============================" );
 
-		logger->log( log_level, "==============================" );
-		logger->log( log_level, "Expression: {}", condition_string );
-		logger->log( log_level, "File: {}", local_path );
-		logger->log( log_level, "Line: {}", line );
-		logger->log( log_level, "==============================" );
+	force_break( should_break );
 
-		force_break( should_break );
-	}
-
-	return condition;
+	return true;
 }
 }
