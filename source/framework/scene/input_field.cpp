02c665348db559aee260d8369f04c98f0005bfd8
condzi <Condzi@users.noreply.github.com>
2019-12-25 19:43:29 +0100

    change the declaration of input_field

--- source/framework/scene/input_field.cpp
+++ source/framework/scene/input_field.cpp
@@ -14,11 +14,4 @@ auto Input_Field::get_input_string() const -> std::string const&
 	return input_string;
 }
 
-void Input_Field::draw_gui( Drawing_Set& drawing_set )
-{
-	if ( not visible ) 
-		return;
-
-	drawing_set.add_drawable( background_shape, layer );
-}
 }
--- source/framework/scene/input_field.hpp
+++ source/framework/scene/input_field.hpp
@@ -19,33 +19,25 @@ public:
 	// When user updates input field this is emitted with parent Input_Field as pointer.
 	Signal<Input_Field const* const> s_input_changed;
 
-	s32  layer{ 0 };
-	bool visible{ true };
+	s32 character_limit{ 4 };
+	Rich_Text* text_visual;
 
 	Input_Field();
-	
-	[[nodiscard]] auto get_input_string() const -> std::string const&;
 
-	void set_background_color_default( sf::Color const& color );
-	void set_background_color_selected( sf::Color const& color );
+	[[nodiscard]] auto get_input_string() const  -> std::string const&;
+
 	void set_text_color_default( sf::Color const& color );
 	void set_text_color_input( sf::Color const& color );
 	void set_default_string( std::string const& str );
-	void set_character_limit( s32 limit );
-	void set_character_size( s32 size );
 
 	void input( sf::Event const& event ) override;
 	void update( r32 dt ) override;
-	void draw_gui( Drawing_Set& drawing_set ) override;
 
 private:
 	enum
 	{
 		Idle, Selected
-	} status = IDLE;
-
-	sf::Color background_color_default;
-	sf::Color background_color_selected;
+	} status = Idle;
 
 	sf::Color text_color_default;
 	sf::Color text_color_input;
@@ -53,9 +45,5 @@ private:
 	std::string input_string;
 	std::string default_string;
 
-	s32 character_limit{ 4 };
-
-	sf::RectangleShape background_shape;
-	Rich_Text* input_text;
 };
 }
