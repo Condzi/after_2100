64e7db01d2a0db56294c28d3381fced4c30eaca7
condzi <Condzi@users.noreply.github.com>
2020-07-06 15:33:09 +0200

    add test for get_all_section_hashes

--- tests/source/engine/config_file_test.cpp
+++ tests/source/engine/config_file_test.cpp
@@ -1,5 +1,6 @@
 #include <testing_utilities.hpp>
 #include <engine/config_file.hpp>
+#include <engine/algorithms.hpp>
 
 TEST_CASE( "config_file", "engine" )
 {
@@ -12,16 +13,31 @@ TEST_CASE( "config_file", "engine" )
 		Config_File cfg;
 		cfg.parse_from_file( CON_CONFIG_FILE );
 
-		CString const vsync = "true";
+		CString const vsync		  = "true";
 		CString const window_size = "1920 1080";
 		CString const dev_console = "false";
-		constant vsync_ = cfg.get_value( "display"_hcs, "vsync"_hcs );
+
+		constant vsync_		  = cfg.get_value( "display"_hcs, "vsync"_hcs );
 		constant window_size_ = cfg.get_value( "display"_hcs, "window_size"_hcs );
 		constant dev_console_ = cfg.get_value( "debug"_hcs, "dev_console"_hcs );
 
-		con_assert( vsync == vsync_ );
-		con_assert( window_size == window_size_ );
-		con_assert( dev_console == dev_console_ );
+		REQUIRE( vsync == vsync_ );
+		REQUIRE( window_size == window_size_ );
+		REQUIRE( dev_console == dev_console_ );
+
+
+		constant display_hash  = "display"_hcs.hash;
+		constant gameplay_hash = "gameplay"_hcs.hash;
+		constant debug_hash    = "debug"_hcs.hash;
+
+		constant sections_array = cfg.get_all_section_hashes();
+
+		REQUIRE( sections_array.size() == 3 );
+
+		REQUIRE( linear_find( sections_array, display_hash ).found()  == true );
+		REQUIRE( linear_find( sections_array, gameplay_hash ).found() == true );
+		REQUIRE( linear_find( sections_array, debug_hash ).found()	  == true );
+
 
 		cfg.free();
 	}
