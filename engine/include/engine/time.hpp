c97179742b701b898a042586833a49adf9f21b73
condzi <Condzi@users.noreply.github.com>
2020-07-30 23:28:07 +0200

    time.hpp now uses 'returning' instead of 'nodiscard auto' style

--- engine/include/engine/time.hpp
+++ engine/include/engine/time.hpp
@@ -11,16 +11,16 @@ using Time_Period = std::chrono::microseconds;
 
 namespace Time
 {
-[[nodiscard]] auto now() -> Time_Period;
+returning now() -> Time_Period;
 
-[[nodiscard]] auto to_microseconds( Time_Period const& time_period ) -> s64;
-[[nodiscard]] auto to_milliseconds( Time_Period const& time_period ) -> s32;
-[[nodiscard]] auto to_seconds( Time_Period const& time_period )		 -> f32;
+returning to_microseconds( Time_Period const& time_period ) -> s64;
+returning to_milliseconds( Time_Period const& time_period ) -> s32;
+returning to_seconds( Time_Period const& time_period )		-> f32;
 
-[[nodiscard]] auto from_microseconds( s64 time_period ) -> Time_Period;
-[[nodiscard]] auto from_milliseconds( s32 time_period ) -> Time_Period;
-[[nodiscard]] auto from_seconds( f32 time_period )		-> Time_Period;
+returning from_microseconds( s64 time_period ) -> Time_Period;
+returning from_milliseconds( s32 time_period ) -> Time_Period;
+returning from_seconds( f32 time_period )	   -> Time_Period;
 
-[[nodiscard]] auto difference( Time_Period const& start, Time_Period const& end ) -> Time_Period;
+returning difference( Time_Period const& start, Time_Period const& end ) -> Time_Period;
 }
 }
