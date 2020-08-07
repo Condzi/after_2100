7ac517eaa2ba1dafda023a6eb6cc0d24f725d80d
condzi <Condzi@users.noreply.github.com>
2020-08-07 13:41:28 +0200

    nicer formatting in macro_config.hpp (use spaces instead of tabs)

--- docs/tasks.md
+++ docs/tasks.md
@@ -3,17 +3,17 @@
     * just simply display text using the new `construct_text` function.
     * adapt solutions from the legacy code. it worked quite nicely. 
 
-# **Debug Console**
+# **Dev Console**
 We initialize it in two steps. 
 * First, set up the buffer to save the messages. Do this as early as possible to not miss the initialization logs. (probably still miss some?)
-* Second, initialize the visuals. Text rendering, backgrounds.
+* Second, initialize the visuals. Text rendering, backgrounds because we need OpenGL context for that.
 
 We could skip that by flushing after initialization of the `Window`, but that would be not so robust. We would have higher chance of missing logs if a sudden crash occurs. That's why we're flushing as frequent as possible.
 
 We need a simple rectangle shapes to create console background.
 * Just add a colored vertex structure. Then `create_rectangle_shape`. Just remember that we're dealing with origin point in the center of the rectangle. Remember to resize the size accordingly to the window's.
 
-The `Debug_Console` class will spawn entities (for background and probably input field in the future), but will not be an entity by itself. We will use a separate `Render_Info` which will be rendered only if the `dev_console` flag (in `local.variables` file) is set. This way we can initialize the class pretty early on and not miss many messages.
+The `Dev_Console` won't use entities at all. The `Renderer` will render its `Render_Info`s at the end of the frame. It's just a special case. There is no need for creating entities in the `Entity_Manager` for that. We need one `Render_Info` for the text, one for the background, for now. We'll need at least two more for the input field.
 
 ----
 ## **other**
--- engine/include/engine/macro_config.hpp
+++ engine/include/engine/macro_config.hpp
@@ -10,9 +10,9 @@
 #define CON_CONCAT_INTERNAL( x, y )  x##y
 #define CON_CONCAT( x, y )  CON_CONCAT_INTERNAL( x, y )
 
-#define CON_BYTES( x )		 x
-#define CON_KILOBYTES( x )	 CON_BYTES( x ) * 1000
-#define CON_MEGABYTES( x )	 CON_KILOBYTES( x ) * 1000
+#define CON_BYTES( x )       ( x )
+#define CON_KILOBYTES( x )   ( x << 10 )
+#define CON_MEGABYTES( x )   ( x << 20 )
 
 
 //
@@ -25,15 +25,16 @@
 #define CON_DEBUG true
 #endif
 
-#define CON_VERSION_STRING "1.0"
-#define CON_WINDOW_TITLE   "after_2100 / v" CON_VERSION_STRING " / Compiled on " __DATE__ " at " __TIME__ " CET"
+#define CON_VERSION_STRING  "1.0"
+#define CON_WINDOW_TITLE    "after_2100 / v" CON_VERSION_STRING " / Compiled on " __DATE__ " at " __TIME__ " CET"
 
 #define CON_ASSERT_MESSAGE_BUFFER_SIZE 512
 
-#define CON_RESERVED_MEMORY  CON_MEGABYTES( 64 )
-#define CON_TEMPORARY_STORAGE_RESERVED_MEMORY  CON_MEGABYTES( 4 )
-#define CON_STACK_RESERVED_MEMORY  CON_KILOBYTES( 128 )
-#define CON_LOGGER_BUFFER_RESERVED_MEMORY  CON_MEGABYTES( 1 )
+#define CON_RESERVED_MEMORY                     CON_MEGABYTES( 64 )
+#define CON_TEMPORARY_STORAGE_RESERVED_MEMORY   CON_MEGABYTES( 4 )
+#define CON_STACK_RESERVED_MEMORY               CON_KILOBYTES( 128 )
+#define CON_LOGGER_BUFFER_RESERVED_MEMORY       CON_MEGABYTES( 1 )
+#define CON_DEV_CONSOLE_BUFFER_RESERVED_MEMORY  CON_LOGGER_BUFFER_RESERVED_MEMORY
 
 #define CON_REQUEST_DEDICATED_GPU  true
 
@@ -50,53 +51,52 @@
 //
 // Data files extensions
 //
-#define CON_LOG_FILE_EXTENSION  ".txt"
-#define CON_CONFIG_FILE_EXTENSION  ".variables"
+#define CON_LOG_FILE_EXTENSION              ".txt"
+#define CON_CONFIG_FILE_EXTENSION           ".variables"
 #define CON_SCENE_RESOURCES_FILE_EXTENSION  ".scene_resources"
-#define CON_SHADERS_FILE_EXTENSION  ".shaders"
+#define CON_SHADERS_FILE_EXTENSION          ".shaders"
 
 //
 // Paths relative to run tree.
 //
-#define CON_LOGS_FOLDER			"logs\\"
-#define CON_DATA_FOLDER			"data\\"
+#define CON_LOGS_FOLDER         "logs\\"
+#define CON_DATA_FOLDER         "data\\"
 
-#define CON_ASSETS_FOLDER		CON_DATA_FOLDER   "assets\\"
-#define CON_SCENES_FOLDER		CON_ASSETS_FOLDER "scenes\\"
-#define CON_TEXTURES_FOLDER		CON_ASSETS_FOLDER "textures\\"
-#define CON_FONTS_FOLDER		CON_ASSETS_FOLDER "fonts\\"
-#define CON_SHADERS_FOLDER		CON_ASSETS_FOLDER "shaders\\"
+#define CON_ASSETS_FOLDER       CON_DATA_FOLDER   "assets\\"
+#define CON_SCENES_FOLDER       CON_ASSETS_FOLDER "scenes\\"
+#define CON_TEXTURES_FOLDER     CON_ASSETS_FOLDER "textures\\"
+#define CON_FONTS_FOLDER        CON_ASSETS_FOLDER "fonts\\"
+#define CON_SHADERS_FOLDER      CON_ASSETS_FOLDER "shaders\\"
 
-#define CON_CONFIG_FILE			CON_DATA_FOLDER "local" CON_CONFIG_FILE_EXTENSION
-#define CON_DEFAULT_LOG_FILE	CON_LOGS_FOLDER "log" CON_LOG_FILE_EXTENSION
+#define CON_CONFIG_FILE         CON_DATA_FOLDER "local" CON_CONFIG_FILE_EXTENSION
+#define CON_DEFAULT_LOG_FILE    CON_LOGS_FOLDER "log" CON_LOG_FILE_EXTENSION
 #define CON_DEFAULT_SCENE_RESOURCES_INFO_FILE   CON_SCENES_FOLDER "default" CON_SCENE_RESOURCES_FILE_EXTENSION
-#define CON_ASSETS_CONFIG_FILE  CON_DATA_FOLDER "assets" CON_CONFIG_FILE_EXTENSION
+#define CON_ASSETS_CONFIG_FILE   CON_DATA_FOLDER "assets" CON_CONFIG_FILE_EXTENSION
 #define CON_PLANETS_CONFIG_FILE  CON_DATA_FOLDER "planets" CON_CONFIG_FILE_EXTENSION
 
 
 //
 // Entities stuff
 //
-#define CON_ENTITIES_LIMIT 128
+#define CON_ENTITIES_LIMIT   128
 #define CON_MAX_ENTITY_FLAGS 8
 
-#define CON_DEBUG_ENTITIES_LIMIT 4
-#define CON_PLANETS_LIMIT 8
+#define CON_DEBUG_ENTITIES_LIMIT  4
+#define CON_PLANETS_LIMIT         8
 
 //
 // Config file stuff
 //
-#define CON_CONFIG_SECTION_MARK ":/"
-#define CON_CONFIG_COMMENT_MARK "#"
+#define CON_CONFIG_SECTION_MARK  ":/"
+#define CON_CONFIG_COMMENT_MARK  "#"
 
 //
 // We're using this values to estimate how much space do we need at max. We do this to allocate
 // only once and then "shrink" the array (by freeing the memory).
 // 
 
-#define CON_MAX_SCENES_IN_FOLDER 32
-#define CON_MAX_CONFIG_ENTRIES   64
-
+#define CON_MAX_SCENES_IN_FOLDER  32
+#define CON_MAX_CONFIG_ENTRIES    64
 
 // Fallback texture color in range <0, 255>
 #define CON_FALLBACK_TEXTURE_COLOR_R  255
