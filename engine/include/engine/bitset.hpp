693e4232d4ca89993f124922f24093319f3ef907
condzi <Condzi@users.noreply.github.com>
2020-08-19 12:10:43 +0200

    cleaner bitset implementation

--- engine/include/engine/bitset.hpp
+++ engine/include/engine/bitset.hpp
@@ -2,193 +2,45 @@
 
 #include <engine/assert.hpp>
 #include <engine/typedefs.hpp>
-#include <engine/context.hpp>
 
-#include <cstring>
-#include <limits>
 
 namespace con
 {
-// @ToDo: make them internal / file_scope?
-returning count_set_bits( u16 value ) -> s32;
-returning find_first_unset_bit( u16 value ) -> Find_Result;
+class Allocator;
 
-// @Idea: don't use template at all, just `byte`? Or maybe u16?
-// @Important: Use unsigned types as TBaseType!!!
-template <typename TBaseType = u16>
-class Bitset_Base final
+// We use con_cassert because it's a core functionality. It's being used in allocators.
+class Bitset final
 {
 public:
-	compile_constant BASE_TYPE_SIZE_IN_BYTES = static_cast<s32>( sizeof( TBaseType ) );
-	compile_constant BASE_TYPE_SIZE_IN_BITS = BASE_TYPE_SIZE_IN_BYTES * 8;
+	compile_constant base_type_size_in_bytes = static_cast<s32>( sizeof( u64 ) );
+	compile_constant base_type_size_in_bits  = base_type_size_in_bytes * 8;
 
-	Bitset_Base() = default;
-
-	void initialize( s32 size_, Allocator* allocator_ = Context.stack_allocator );
+	// size_ is the size in bits.
+	void initialize( s32 size_, Allocator* allocator_ );
 	void shutdown();
 
-	void set( s32 idx );
+	void set(   s32 idx );
 	void reset( s32 idx );
-	void set_range( s32 idx, s32 size );
+
+	void set_range(   s32 idx, s32 size );
 	void reset_range( s32 idx, s32 size );
 
 	void flip( s32 idx );
 	void clear();
+
 	returning test( s32 idx ) const -> bool;
 
 	returning find_first_unset_bit( s32 begin = 0 ) const -> Find_Result;
 	returning count_set_bits() const -> s32;
 
 private:
-	Allocator* allocator = nullptr;
-	s32 size_in_bits = -1;
+	Allocator* allocator   = nullptr;
+	u64* data              = nullptr;
+	s32 size_in_bits       = -1;
 	s32 size_in_base_types = -1;
-	TBaseType* data = nullptr;
+
+	// @Robustness: consider adding `data_size_in_bytes` to the Bitset.
+	// We often use `base_type_size_in_bytes * size_in_base_types` combo, which
+	// is equal to size of `data` in bytes. -- condzi, 12.08.2020
 };
-
-// Using u16 allows us to use compiler intrinsic functions.
-using Bitset = Bitset_Base<u16>;
-
-//
-// Definitions
-//
-
-template <typename TBaseType>
-void Bitset_Base<TBaseType>::initialize( s32 size_, Allocator* allocator_ )
-{
-	con_assert( size_ > 0 );
-	con_assert( allocator_ != nullptr );
-	con_assert( data == nullptr );
-
-	allocator = allocator_;
-	size_in_bits = size_;
-	size_in_base_types = size_in_bits / BASE_TYPE_SIZE_IN_BITS + 1;
-
-	data = reinterpret_cast<TBaseType*>( allocator->allocate( BASE_TYPE_SIZE_IN_BYTES * size_in_base_types ) );
-
-	memset( data, 0, BASE_TYPE_SIZE_IN_BYTES * size_in_base_types );
-}
-
-template <typename TBaseType>
-void Bitset_Base<TBaseType>::shutdown()
-{
-	if ( data == nullptr ) {
-		return;
-	}
-
-	allocator->free( reinterpret_cast<byte*>( data ), BASE_TYPE_SIZE_IN_BYTES * size_in_base_types );
-	data = nullptr;
-	size_in_bits = -1;
-	size_in_base_types = -1;
-	allocator = nullptr;
-}
-
-template <typename TBaseType>
-void Bitset_Base<TBaseType>::set( s32 idx )
-{
-	con_assert( idx < size_in_bits );
-	data[idx / BASE_TYPE_SIZE_IN_BITS] |= ( 1 << ( idx%BASE_TYPE_SIZE_IN_BITS ) );
-}
-
-template <typename TBaseType>
-void Bitset_Base<TBaseType>::reset( s32 idx )
-{
-	con_assert( idx < size_in_bits );
-	data[idx / BASE_TYPE_SIZE_IN_BITS] &= ~( 1 << ( idx%BASE_TYPE_SIZE_IN_BITS ) );
-}
-
-template <typename TBaseType>
-void Bitset_Base<TBaseType>::set_range( s32 idx, s32 size )
-{
-	// @Performance: we probably can do better than that (setting every byte instead of every individual
-	// bit), but we don't need such microoptimalizations right now
-
-	con_assert( idx + size < size_in_bits );
-	for ( s32 i = idx; i < idx + size; ++i ) {
-		data[i / BASE_TYPE_SIZE_IN_BITS] |= ( 1 << ( i%BASE_TYPE_SIZE_IN_BITS ) );
-	}
-}
-
-template <typename TBaseType>
-void Bitset_Base<TBaseType>::reset_range( s32 idx, s32 size )
-{
-	con_assert( idx + size < size_in_bits );
-	for ( s32 i = idx; i < idx + size; ++i ) {
-		data[i / BASE_TYPE_SIZE_IN_BITS] &= ~( 1 << ( i%BASE_TYPE_SIZE_IN_BITS ) );
-	}
-}
-
-template <typename TBaseType>
-void Bitset_Base<TBaseType>::flip( s32 idx )
-{
-	con_assert( idx < size_in_bits );
-	data[idx/ BASE_TYPE_SIZE_IN_BITS] ^= ( 1 << idx%BASE_TYPE_SIZE_IN_BITS );
-}
-
-template <typename TBaseType>
-void Bitset_Base<TBaseType>::clear()
-{
-	memset( data, 0, BASE_TYPE_SIZE_IN_BYTES * size_in_base_types );
-}
-
-template <typename TBaseType>
-auto Bitset_Base<TBaseType>::test( s32 idx ) const -> bool
-{
-	con_assert( idx < size_in_bits );
-	// @Performance: we can use intrinsic here _bittest
-	return ( data[idx / BASE_TYPE_SIZE_IN_BITS] & ( 1 << ( idx % BASE_TYPE_SIZE_IN_BITS ) ) );
-}
-
-template<typename TBaseType>
-returning Bitset_Base<TBaseType>::find_first_unset_bit( s32 begin ) const -> Find_Result
-{
-	// @Robustness: just change it to u16 and everything will be simpler. For real,
-	// we have no real reason to keep u8/u16 as default now.
-	static_assert( std::is_same_v<u16, TBaseType>, "find_first_unset_bit is working only for u16" );
-
-	// We loop until we get to k * BASE_TYPE_SIZE_IN_BITS, then we can safelly
-	// use the find_first_unset_bit function.
-
-	s32 bit = begin;
-	for ( ; bit % BASE_TYPE_SIZE_IN_BITS != 0 && bit < size_in_bits; ++bit ) {
-		if ( test( bit ) == false ) {
-			return { .idx = bit };
-		}
-	}
-
-	if ( bit > size_in_bits-1 ) {
-		return { .idx = -1 };
-	}
-
-	s32 u16_idx = bit / BASE_TYPE_SIZE_IN_BITS;
-	for ( ; u16_idx < size_in_base_types; ++u16_idx ) {
-		constant result = con::find_first_unset_bit( data[u16_idx] );
-
-		if ( result.found() ) {
-			// We may have exceed declared size in bits (but we never do that with size_in_base_types)
-			// so we have to check for that.
-			constant bit_to_return = u16_idx * BASE_TYPE_SIZE_IN_BITS + result.idx;
-			if ( bit_to_return >= size_in_bits ) {
-				return { .idx = -1 };
-			} else {
-				return { .idx = bit_to_return };
-			}
-		}
-	}
-
-	return { .idx = -1 };
-}
-
-template <typename TBaseType>
-returning Bitset_Base<TBaseType>::count_set_bits() const -> s32
-{
-	static_assert( std::is_same_v<u16, TBaseType>, "count_set_bits is working only for u16" );
-
-	s32 sum = 0;
-	for ( s32 i = 0; i < size_in_base_types; ++i ) {
-		sum += con::count_set_bits( data[i] );
-	}
-
-	return sum;
-}
 }
--- engine/source/engine/bitset.cpp
+++ engine/source/engine/bitset.cpp
@@ -1,44 +1,174 @@
+#include <engine/allocator.hpp>
 #include <engine/bitset.hpp>
 
+#include <cstring>
 #include <intrin.h>
 
-namespace con
+//
+// Wrappers for MSVC intrinsics.
+//
+namespace con::priv
 {
-returning count_set_bits( u16 value ) -> s32
+returning count_set_bits( u64 value ) -> s32
 {
-	// msvc specific
-	return static_cast<s32>( __popcnt16( static_cast<unsigned short>( value ) ) );
+	return static_cast<s32>( static_cast<unsigned __int64>( value ) );
 }
 
-returning find_first_unset_bit( u16 value ) -> Find_Result
+returning find_first_unset_bit( u64 value ) -> Find_Result
 {
-	if ( value == 0 ) {
+	if ( value == 0 ){
 		return { .idx = 0 };
 	}
 
 	unsigned long idx = 0;
+	auto value_casted =  static_cast<unsigned __int64>( value );
+	// We have to negate it because _BitScanForward looks for
+	// first set bit -- we need first unset one.
+	value_casted = ~value_casted;
 
-
-	/*
-	 First, we convert u16 value to u32
-
-			11111111111111110000000000000000 or
-			00000000000000001000000000000000 =
-			11111111111111111000000000000000
-
-	 Then we negate it to make it work with _BitScan which
-	 looks for first *set* bit (we're looking for unset one):
-
-			11111111111111111000000000000000 not
-			00000000000000000111111111111111
-	*/
-	compile_constant mask = 0xFFFF0000; /* u32( 0b11111111111111110000000000000000 ) */
-	constant value_to_check = ~( static_cast<unsigned long>( value ) | mask );
-
-	if ( _BitScanForward( &idx, value_to_check ) == 0 ) {
-		return { .idx  = -1 };
+	if ( _BitScanForward64( &idx, value_casted ) == 0 ){
+		return { .idx = -1 };
 	}
 
 	return { .idx = static_cast<s32>( idx ) };
+}
+}
+
+namespace con
+{
+void Bitset::initialize( s32 size_, Allocator* allocator_ )
+{
+	con_assert( size_ > 0 );
+	con_assert( allocator_ != nullptr );
+	// Fires if Bitset was already initialized. Or we have garbage in the memory.
+	con_assert( data == nullptr );
+
+	allocator          = allocator_;
+	size_in_bits       = size_;
+	size_in_base_types = ( size_in_bits / base_type_size_in_bits ) + 1;
+
+	data = reinterpret_cast<u64*>( allocator->allocate( base_type_size_in_bytes * size_in_base_types ) );
+
+	memset( data, 0, base_type_size_in_bytes * size_in_base_types );
+}
+
+void Bitset::shutdown()
+{
+	if ( data == nullptr ){
+		return;
+	}
+
+	allocator->free( reinterpret_cast<byte*>( data ), base_type_size_in_bytes * size_in_base_types );
+
+	data               = nullptr;
+	allocator          = nullptr;
+	size_in_bits       = -1;
+	size_in_base_types = -1;
+}
+
+void Bitset::set( s32 idx )
+{
+	con_assert( idx >= 0 );
+	con_assert( idx < size_in_bits );
+
+	data[idx / base_type_size_in_bits] |= ( 1ull << ( idx % base_type_size_in_bits ) );
+}
+
+void Bitset::reset( s32 idx )
+{
+	con_assert( idx >= 0 );
+	con_assert( idx < size_in_bits );
+
+	data[idx / base_type_size_in_bits] &= ~( 1ull << ( idx%base_type_size_in_bits ) );
+}
+
+void Bitset::set_range( s32 idx, s32 size )
+{
+	con_assert( idx >= 0 );
+	con_assert( idx + size < size_in_bits );
+
+	// @Performance: set every u64 instead of individual bits.
+	// We don't need such micro-optimisation right now. -- condzi, 12.08.2020
+
+	for ( s32 i = idx; i < idx + size; ++i ){
+		data[i / base_type_size_in_bits] |= ( 1ull << ( i % base_type_size_in_bits ) );
+	}
+}
+
+void Bitset::reset_range( s32 idx, s32 size )
+{
+	con_assert( idx >= 0 );
+	con_assert( idx + size < size_in_bits );
+
+	for ( s32 i = idx; i < idx + size; ++i ){
+		data[i / base_type_size_in_bits] &= ~( 1ull << ( i%base_type_size_in_bits ) );
+	}
+}
+
+void Bitset::flip( s32 idx )
+{
+	con_assert( idx >= 0 );
+	con_assert( idx < size_in_bits );
+
+	data[idx / base_type_size_in_bits] ^= ( 1ull << ( idx%base_type_size_in_bits ) );
+}
+
+void Bitset::clear()
+{
+	memset( data, 0, base_type_size_in_bytes * size_in_base_types );
+}
+
+returning Bitset::test( s32 idx ) const -> bool
+{
+	con_assert( idx >= 0 );
+	con_assert( idx < size_in_bits );
+
+	return data[idx / base_type_size_in_bits] & ( 1ull << ( idx%base_type_size_in_bits ) );
+}
+
+returning Bitset::find_first_unset_bit( s32 begin ) const -> Find_Result
+{
+	// We loop until we get to k * BASE_TYPE_SIZE_IN_BITS, then we can safelly
+	// use the find_first_unset_bit function.
+
+	s32 bit = begin;
+	for ( ; bit % base_type_size_in_bits != 0 && bit < size_in_bits; ++bit ) {
+		if ( test( bit ) == false ) {
+			return { .idx = bit };
+		}
+	}
+
+	if ( bit >= size_in_bits-1 ) {
+		return { .idx = -1 };
+	}
+
+	s32 u64_idx = bit / base_type_size_in_bits;
+	for ( ; u64_idx < size_in_base_types; ++u64_idx ) {
+		constant result = con::priv::find_first_unset_bit( data[u64_idx] );
+
+		if ( result.found() ) {
+			// We may have exceed declared size in bits (but we never do that with size_in_base_types)
+			// so we have to check for that.
+			constant bit_to_return = u64_idx * base_type_size_in_bits + result.idx;
+			if ( bit_to_return >= size_in_bits ) {
+				return { .idx = -1 };
+			} else {
+				return { .idx = bit_to_return };
+			}
+		}
+	}
+
+	return { .idx = -1 };
+}
+
+returning Bitset::count_set_bits() const -> s32
+{
+	s32 sum = 0;
+
+	for ( s32 i = 0; i < size_in_base_types; ++i ) {
+		sum += con::priv::count_set_bits( data[i] );
+	}
+
+	return sum;
 }
 }
